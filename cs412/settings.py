af1a90cc6edc45833cfce665e831f3759c16273d
annaandmandy <annaandtommy1004@gmail.com>
2025-09-15 22:01:22 -0400

    edit setting

--- cs412/settings.py
+++ cs412/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-jsyo4s-s6*p#*m54mz-lk7!d@nye#386#r*pv(a79*vdjcjdyz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["cs-webapps.bu.edu", "127.0.0.1"]
 
 
 # Application definition
@@ -120,13 +120,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = 'static/' # note: no leading slash!
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL= "media/"  # note: no leading slash!
+
+import socket
+CS_DEPLOYMENT_HOSTNAME = 'cs-webapps.bu.edu'
+
+if socket.gethostname() == CS_DEPLOYMENT_HOSTNAME:
+    STATIC_URL = '/huanghy/static/'
+    MEDIA_URL = '/huanghy/media/'
