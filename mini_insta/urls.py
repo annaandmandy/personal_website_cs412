9f7a9375936f9dc0fdedb9c65991ee011618c36c
annaandmandy <annaandtommy1004@gmail.com>
2025-09-23 11:57:32 -0400

    change profile url

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.ProfileListView.as_view(), name='profiles'),
-    path('profile/<str:username>/', views.ProfileDetailView.as_view(), name='show_profile')
+    path('profile/<int:pk>/', views.ProfileDetailView.as_view(), name='show_profile')
 ]
